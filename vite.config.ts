8d8d91d40845d27e93a1b7f43ce8789c5fdcec4f
SoraKumo <info@croud.jp>
2025-03-31 09:18:55 +0900

    chore: Update package.json dependencies and reintroduce wasm-image-optimization plugin in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import adapter from "@hono/vite-dev-server/cloudflare";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import serverAdapter from "hono-react-router-adapter/vite";
-import wasmImageOptimizationPlugin from "wasm-image-optimization/vite-plugin";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import wasmImageOptimizationPlugin from "wasm-image-optimization/vite-plugin";
 
 const entry = "./workers/app.ts";
 
